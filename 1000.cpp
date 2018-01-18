1878a73da8b4974a2959104885bd5008e6204d31
NashWang1997 <34232770+NashWang1997@users.noreply.github.com>
2018-01-18 20:42:40 +0800

    Add files via upload

---
+++ 1000.cpp
@@ -0,0 +1,26 @@
+/*
+1000. A+B Problem
+Description
+作为所有 Online Judge 的传统题目，你只需读两个整数，输出即可，保证输入的数绝对值不超过1000。
+
+Input Format
+一行,两个空格隔开的整数A,B。
+
+Output Format
+一个数A+B。
+
+Sample Input
+3 2
+Sample Output
+5
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+    return 0;
+}
